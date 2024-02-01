85c3d3ef746c5ba0b30d307b781b8fa1b099167a
Michael Sarfo <43896811+comboed@users.noreply.github.com>
2024-01-31 23:29:56 -0500

    Update util.go

--- util.go
+++ util.go
@@ -1,56 +1,25 @@
 package main
 
-type Request struct {
-	Packet string
-	Headers map[string]string
-	SetHeaders []string
+import (
+	"strings"
+)
 
-}
-
-func createRequest() *Request {
-	return &Request {
-		Headers: make(map[string]string),
+func getScheme(URL string) string {
+	var index int = strings.Index(URL, "://")
+	if index >= 4 {
+		var scheme string = URL[:index]
+		if scheme == "http" || scheme == "https" {
+			return scheme
+		}
 	}
+	panic("Missing URL Scheme")
 }
 
-func (request *Request) SetMethod(method string) {
-	request.Headers["Method"] = method
+func getHost(URL, scheme string) string {
+	var noSchemeURL string = URL[len(scheme + "://"):]
+	return noSchemeURL[:strings.Index(noSchemeURL, "/")]
 }
 
-func (request *Request) SetURL(URL string) {
-	request.Headers["URL"] = URL
+func isTLS(URL string) bool {
+	return getScheme(URL) == "https"
 }
-
-func (request *Request) SetHost(host string) {
-	request.Headers["Host"] = host
-}
-
-func (request *Request) SetBody(body string) {
-	request.Headers["Body"] = body
-}
-
-func (request *Request) Set(key, value string) {
-	request.Headers[key] = value
-}
-
-func (request *Request) DeleteHeader(header string) {
-	delete(request.Headers, header)
-}
-
-func (request *Request) IsHeaderSet(key string) {
-	
-}
-
-func parseHeaders(request *Request) {
-	if method := request.Headers["Method"]; method != "" {
-		request.Packet += method
-	} else {
-		request.Headers["Method"] = "GET" // assume it was a GET request
-	}
-	if URL := request.Headers["URL"]; URL != "" {
-		request.Packet += " " + URL + " HTTP/1.1\r\n"
-	} else {
-		panic("No URL supplied")
-	}
-}
-
