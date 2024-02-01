43968b535c0d9d59508c2623fe77dd76e9791d4b
Michael Sarfo <43896811+comboed@users.noreply.github.com>
2024-01-31 23:55:18 -0500

    Update requests.go

--- requests.go
+++ requests.go
@@ -1,23 +1,59 @@
 package main
 
 type Request struct {
-	Packet map[string]string
-
+	Packet string
+	Headers map[string]string
+	Body string
 }
+
 func createRequest() *Request {
 	return &Request {
-		Packet: make(map[string]string),
+		Headers: make(map[string]string),
 	}
 }
 
-func (request *Request) Set(header, value string) {
-	request.Packet[header] = value
+func (request *Request) SetMethod(method string) {
+	request.Headers["Method"] = method
+}
+
+func (request *Request) SetURL(URL string) {
+	request.Headers["URL"] = URL
+}
+
+func (request *Request) SetHost(host string) {
+	request.Headers["Host"] = host
 }
 
 func (request *Request) SetBody(body string) {
-	request.Packet["Body"] = body
+	request.Headers["Body"] = body
+}
+
+func (request *Request) SetHeader(key, value string) {
+	request.Headers[key] = value
 }
 
 func (request *Request) DeleteHeader(header string) {
-	delete(request.Packet, header)
+	delete(request.Headers, header)
+}
+
+func parseHeaders(request *Request) {
+	if method := request.Headers["Method"]; method != "" {
+		request.Packet = method
+	} else {
+		request.Packet = "GET" // assume it was a GET request
+	}
+	if URL := request.Headers["URL"]; URL != "" {
+		request.Packet += " " + URL + " HTTP/1.1\r\n"
+	} else {
+		panic("No URL supplied")
+	}
+	if host := request.Headers["Host"]; host != "" {
+		request.Packet += "Host: " + host + "\r\n"
+	}
+	for key, value := range request.Headers {
+		if key != "Method" && key != "URL" && key != "Host" {
+			request.Packet += key + ": " + value + "\r\n"
+		}
+	}
+	request.Packet += "\r\n"
 }
