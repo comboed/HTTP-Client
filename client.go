a52bc6ed778cd15c846afdfa5e30bd8be3fc5f82
Michael Sarfo <43896811+comboed@users.noreply.github.com>
2024-02-01 14:27:38 -0500

    Update client.go

--- client.go
+++ client.go
@@ -1,2 +1,39 @@
 package main
 
+import (
+	"crypto/tls"
+	_"net"
+)
+
+type Client struct {
+	ReadBufferSize int
+	MaxConnections int
+	Connections []*tls.Conn
+	ConnectionPool chan []*tls.Conn
+}
+
+func createClient(readBufferSize, MaxConnections int) *Client {
+	return &Client {
+		ReadBufferSize: readBufferSize,
+		MaxConnections: MaxConnections,
+		Connections: []*tls.Conn{},
+		ConnectionPool: make(chan []*tls.Conn),
+	}
+}
+
+func createConnection(address string) *tls.Conn {
+	var conn, _ = tls.Dial("tcp", address, &tls.Config{})
+	return conn
+}
+
+func appendConnection(connections *[]*tls.Conn, URL string) {
+	if isTLS(URL) {
+		*connections = append(*connections, createConnection(getHost(URL) + ":443"))
+	} else {
+		*connections = append(*connections, createConnection(getHost(URL) + ":80"))
+	}
+}
+
+func fillConnectionPool(connections *[]*tls.Conn) {
+	//appendConnection(connections, "")
+}
