09a156b5a400cbc3af1ab6ae47a22be0c868dc61
Michael Sarfo <43896811+comboed@users.noreply.github.com>
2024-02-01 13:54:34 -0500

    Update util.go

--- util.go
+++ util.go
@@ -15,9 +15,15 @@ func getScheme(URL string) string {
 	panic("Missing URL Scheme")
 }
 
-func getHost(URL, scheme string) string {
-	var noSchemeURL string = URL[len(scheme + "://"):]
-	return noSchemeURL[:strings.Index(noSchemeURL, "/")]
+func getHost(URL string) string {
+	var noSchemeURL string = URL[len(getScheme(URL) + "://"):]
+	var slashIndex, queryIndex int = strings.Index(noSchemeURL, "/"), strings.Index(noSchemeURL, "?")
+	if slashIndex != -1 {
+		return noSchemeURL[:slashIndex]
+	} else if queryIndex != -1 {
+		return noSchemeURL[:queryIndex]
+	}
+	return noSchemeURL
 }
 
 func isTLS(URL string) bool {
