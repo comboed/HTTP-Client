7e11031b50f73b7195e9ea7b23ce2fcc22e9cb3d
Michael Sarfo <43896811+comboed@users.noreply.github.com>
2024-09-27 15:59:37 -0400

    Update client.go
    minor bug fixes

--- client.go
+++ client.go
@@ -18,31 +18,32 @@ func createClient(maxConnections, readBufferSize int, tlsConfig *tls.Config) *Cl
 		MaxConnections: maxConnections,
 		ReadBufferSize: readBufferSize,
 		TLSConfig: tlsConfig,
-		Pool: make(chan *Connection, maxConnections),
+		Pool: make(chan *Connection, maxConnections + 100),
 	}
 }
 
-func (client *Client) GetConnection(request *Request, reDial bool) *Connection {
-	if len(client.Pool) < client.MaxConnections || reDial {
+func (client *Client) GetConnection(request *Request, reDial bool) *Connection {	
+	if len(client.Pool) < client.MaxConnections {
 		var connection *Connection = createConnection(client.TLSConfig)
 		connection.DialHost(request)
 		return connection
-	} else {
-		return <- client.Pool
 	}
+	return <- client.Pool
 }
 
 func (client *Client) Do(request *Request) []byte {
 	var connection *Connection = client.GetConnection(request, false)
 	var response *fasthttp.Response = fasthttp.AcquireResponse()
-	request.buldPacket() // temp for now, figure out a new way to not constantly build packets on Do
 	for {
-		if _, err := connection.Conn.Write([]byte(request.Packet)); err != nil {
+		if _, err := connection.Conn.Write([]byte(request.buldPacket())); err != nil {
+			connection.Conn.Close()
 			connection = client.GetConnection(request, true)
 			continue
 		}
+		if err := response.Read(bufio.NewReader(connection.Conn)); err != nil {
+			continue
+		}
 		client.Pool <- connection
-		response.Read(bufio.NewReader(connection.Conn))
 		return response.Body()
 	}
 }
